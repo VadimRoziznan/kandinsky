4277364792c94c3e0b43375cc3600c97f594ee5f
RoziznanVadim <roziznan_v@mail.ru>
2024-05-31 17:40:50 +0300

    Added a description.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from core.generator import generating_images_all_tasks, generate_task_images
 
 
-# оставьте одну функцию вторую закомментируйте
+# оставьте одну функцию вторую закомментируйте.
 
-generating_images_all_tasks()  # если необходимо совершить генерацию по всем prompt.
+generating_images_all_tasks()  # если необходимо совершить генерацию изображений к всем задачам.
 
 generate_task_images(
     task_name="Задача №6"
-)  # если необходимо совершить генерацию одного prompt.
+)  # если необходимо совершить генерацию одного изображения к конкретной задаче.
